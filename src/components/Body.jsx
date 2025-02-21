812e96b3f1585029e04a7f89a9a11f07082e03b2
abrarr21 <abrarrr2110@gmail.com>
2025-02-21 10:51:01 +0530

    API data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,26 +1,82 @@
 import { useState } from "react";
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filterRestaurant, setFilterRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+    setFilterRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+  };
+
+  if (listOfRestaurant.length === 0) {
+    return (
+      <h1 className="flex justify-center items-center h-screen font-bold text-3xl">
+        Loading....
+      </h1>
+    );
+  }
   return (
     <div className="body m-5">
-      <div className="fiter-bar">
+      <div className="fiter-bar flex gap-5">
+        <div className="search-bar">
+          <input
+            className="border border-black rounded-lg"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            className="ml-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            onClick={() => {
+              const searchedFilter = listOfRestaurant.filter((res) =>
+                res?.info?.name
+                  ?.toLowerCase()
+                  .includes(searchText.toLowerCase()),
+              );
+
+              setFilterRestaurant(searchedFilter);
+              console.log(searchText);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
-          className="filter-btn"
+          className="filter-btn px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           onClick={() => {
-            const filteredList = listOfRestaurant.filter(
-              (res) => res.info.avgRating > 4.5,
+            const filterList = listOfRestaurant.filter(
+              (res) => res?.info?.avgRating > 4.4,
             );
-            setListOfRestaurant(filteredList);
+
+            console.log(filterList);
+            setFilterRestaurant(filterList);
           }}
         >
           Top Rated Restaurant
         </button>
       </div>
       <div className="res-container flex flex-wrap">
-        {listOfRestaurant.map((restaurant) => (
+        {filterRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,9 @@ const Header = () => {
           <li className="p-[10px] m-[10px]">About</li>
           <li className="p-[10px] m-[10px]">Contact</li>
           <li className="p-[10px] m-[10px]">Cart</li>
+          <button className="px-2 bg-blue-500 text-white font-semibold text-sm rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+            Login
+          </button>
         </ul>
       </div>
     </div>
