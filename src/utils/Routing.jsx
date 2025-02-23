812321fe5d233f47da14ddd36ff166bb823eee52
abrarr21 <abrarrr2110@gmail.com>
2025-02-23 13:39:34 +0530

    Added AboutComponent

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import RestuarantMenu from "../components/RestaurantMenu";
 import Body from "../components/Body";
+import About from "../components/About";
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Body />} />
+      <Route path="/about" element={<About />} />
       <Route path="/restaurants/:resId" element={<RestuarantMenu />} />
     </Routes>
   );
