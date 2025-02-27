5e3d6491a63ac1553e12b5b217ce1b07f2f84d00
abrarr21 <abrarrr2110@gmail.com>
2025-02-27 19:38:57 +0530

    clearBtn & CDNfetch

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,6 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import { BODY_URL } from "../utils/constants";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState([]);
@@ -14,12 +15,10 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-    );
+    const data = await fetch(BODY_URL);
 
     const json = await data.json();
-    console.log(json);
+    //console.log(json);
     setListOfRestaurant(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants,
@@ -42,16 +41,29 @@ const Body = () => {
       </h1>
     );
   }
+
+  const handleClearBtn = () => {
+    setSearchText("");
+  };
+
   return (
     <div className="body m-5">
       <div className="fiter-bar flex gap-5">
         <div className="search-bar">
           <input
-            className="border border-black rounded-lg"
+            className="border border-black rounded-lg p-1"
             type="text"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
           />
+          {searchText.length !== 0 && (
+            <span
+              className="ml-2 p-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full cursor-pointer"
+              onClick={handleClearBtn}
+            >
+              X
+            </span>
+          )}
           <button
             className="ml-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             onClick={() => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,6 @@ export const LOGO_URL =
 
 export const MENU_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6139298&lng=77.2088282&restaurantId=";
+
+export const BODY_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139298&lng=77.2088282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
